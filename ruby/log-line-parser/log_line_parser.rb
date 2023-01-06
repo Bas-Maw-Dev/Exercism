94708227f7db62ea1d2259970d86483bbadc9834
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-01-06 11:19:04 +0000

    1st iteration of log-line-parser

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,17 +1,23 @@
 class LogLineParser
   def initialize(line)
     @line = line
+    @colon_position = @line.index(':')
+    @size_of_string = @line.size
   end
 
   def message
-    raise 'Please implement the LogLineParser#message method'
+    @line[@colon_position + 1, @size_of_string].strip
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    level = @line[0, @colon_position -1].scan(/\w+/)
+    level[0].downcase
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    message = @line[@colon_position + 1, @size_of_string].strip
+    levels = @line[0, @colon_position -1].scan(/\w+/)
+    level = levels[0].downcase
+    "#{message} (#{level})"
   end
 end
